df1dad80c72782412308d6488211868aea255002
ahmadharthey <ahmoood@outlook.com>
2026-04-19 23:51:52 +0300

    routing fix

--- api/proxy.js
+++ api/proxy.js
@@ -13,8 +13,8 @@ export default async function handler(req, res) {
   const { path, ...query } = req.query;
 
   const queryString = new URLSearchParams(query).toString();
-
   const url = `http://92.205.234.30:7071/api/${path}`;
+  console.log(url)
 
   try {
     const headers = { ...req.headers };
